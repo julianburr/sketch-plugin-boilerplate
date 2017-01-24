ce0e5e47fb4e631e0ddf0092697e23c57b0939c3
Julian Burr <julian.burr@rexsoftware.com.au>
2017-01-24 23:20:32 +1000

    Add sample component to web view source

---
+++ src/webview/js/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/webview/components/App.js</code> and save to reload. I can see this...
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
