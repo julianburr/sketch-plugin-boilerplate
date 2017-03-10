dfd7700862cc97b113bee0a98cf36674d8d4b820
Julian Burr <julian.burr@rexsoftware.com.au>
2017-03-10 10:00:35 +1000

    Minor framework changes + remove logs

--- src/frameworks/SketchPluginBoilerplate/SPBWebViewMessageUtils.m
+++ src/frameworks/SketchPluginBoilerplate/SPBWebViewMessageUtils.m
@@ -22,27 +22,21 @@
 }
 
 +(void)send:(NSString *)message {
-    // Set packet contents
     [self setPayload:message];
-    // Init method selector for method we want to call on AppController (look at AppControler.h of slack).
+    // Set selector and check if Sketch responses to it to avoid crashes
     SEL runPluginCmdSel = NSSelectorFromString(@"runPluginCommandWithIdentifier:fromBundleAtURL:");
-    // Make sure AppController responds to our selector so sketch doesn't crash. AppController is exposed via [NSApp delegate]
-    // NSApp in this case is the application loading the framework (sketch), not the plugin itself.
-    if( ![[NSApp delegate] respondsToSelector:runPluginCmdSel] ){
-        NSLog(@"Sketch delegate not responding to selector: runPluginCommandWithIdentifier:fromBundleAtURL:");
+    if (![[NSApp delegate] respondsToSelector:runPluginCmdSel]) {
+        NSLog(@"Sketch delegate not responding");
         return;
     }
     // Determine plugin path
-    // NOTE: hard coded as of now :/
+    // NOTE: hard coded plugin file name as of now :/
     NSString *pluginPath = [[[[NSFileManager defaultManager] URLsForDirectory:NSApplicationSupportDirectory inDomains:NSUserDomainMask] firstObject] path];
-    NSLog(@"Plugin Path I: %@", pluginPath);
     pluginPath = [pluginPath stringByAppendingPathComponent:@"com.bohemiancoding.sketch3/Plugins"];
     pluginPath = [pluginPath stringByAppendingPathComponent:@"sketch-plugin-boilerplate.sketchplugin"];
     NSURL *pluginUrl = [NSURL fileURLWithPath:pluginPath];
-    NSLog(@"Plugin Path II: %@", pluginPath);
     // Finally, call the plugin method "handlebridgemessage". On the sketch side, it will read the message and process it
     [[NSApp delegate] performSelector:runPluginCmdSel withObject:@"handleBridgeMessage" withObject:pluginUrl];
-    NSLog(@"Plugin Path II: %@", pluginUrl);
     return;
 }
 
